4b462886b567040f8a76190123e82a1fc6d9b741
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-06-11 10:31:03 +0100

    Initial commit of terraform templates for jx

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "google" {
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+resource "google_container_node_pool" "jx-node-pool" {
+  name       = "default-pool"
+  zone       = "${var.gcp_zone}"
+  cluster    = "${google_container_cluster.jx-cluster.name}"
+  node_count = "${var.min_node_count}"
+
+  node_config {
+    preemptible  = "${var.node_preemptible}"
+    machine_type = "${var.node_machine_type}"
+    disk_size_gb = "${var.node_disk_size}"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+
+  autoscaling {
+	min_node_count = "${var.min_node_count}"
+	max_node_count = "${var.max_node_count}"
+  }
+
+}
+
+resource "google_container_cluster" "jx-cluster" {
+  name               = "${var.cluster_name}"
+  description        = "jx k8s cluster"
+  zone               = "${var.gcp_zone}"
+  enable_kubernetes_alpha = "true"
+  enable_legacy_abac = "true"
+  initial_node_count = "${var.min_node_count}"
+  remove_default_node_pool = "true"
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-b (which must be in gcp_region)"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "cluster_name" {
+  description = "Name of the K8s cluster"
+}
+
+variable "min_node_count" {
+  description = "Min Number of worker VMs to create"
+  default = 3
+}
+
+variable "max_node_count" {
+  description = "Max Number of worker VMs to create"
+  default = 5
+}
+
+variable "node_machine_type" {
+  description = "GCE machine type"
+  default = "n1-standard-2"
+}
+
+variable "node_preemptible" {
+  description = "Use preemptible nodes"
+  default = "false"
+}
+
+variable "node_disk_size" {
+  description = "Node disk size in GB"
+  default = "20"
+}
