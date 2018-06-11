ebfec3869279bba3087260036e5a90bd8f036622
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-06-11 13:09:22 +0100

    Removed unrequired output

--- output.tf
+++ output.tf
@@ -5,15 +5,3 @@ output "cluster_endpoint" {
 output "cluster_master_version" {
   value = "${google_container_cluster.jx-cluster.master_version}"
 }
-
-output "cluster_master_auth_client_certificate" {
-  value = "${google_container_cluster.jx-cluster.master_auth.0.client_certificate}"
-}
-
-output "cluster_master_auth_client_key" {
-  value = "${google_container_cluster.jx-cluster.master_auth.0.client_key}"
-}
-
-output "cluster_master_auth_cluster_ca_certificate" {
-  value = "${google_container_cluster.jx-cluster.master_auth.0.cluster_ca_certificate}"
-}
